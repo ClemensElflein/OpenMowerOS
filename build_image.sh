a62c1557c52b0dd84d05e8fd34838ebb2c144510
Clemens Elflein <clemens1@familie-elflein.de>
2022-11-06 18:53:36 +0100

    feat: add comitup

--- build_image.sh
+++ build_image.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Call this script to build the image
 
-if compgen -G "./OpenMowerOS/src/image/*.zip" > /dev/null; then
+if compgen -G "./OpenMowerOS/src/image/*" > /dev/null; then
     echo "Image exists, skipping download."
 else
     echo "No image found, downloading latest Raspbiann image."
-    bash -c "cd OpenMowerOS/src && mkdir -p image && cd image && wget -c --trust-server-names 'https://downloads.raspberrypi.org/raspbian_lite_latest'"
+    bash -c "cd OpenMowerOS/src && mkdir -p image && cd image && wget -c --trust-server-names 'https://downloads.raspberrypi.org/raspios_lite_arm64_latest'"
 fi
 
 echo "Starting Image Build"
