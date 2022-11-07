1c368ed1fd1ba5f4716d424db5ffba7a407d8db3
Clemens Elflein <clemens1@familie-elflein.de>
2022-11-07 02:46:18 +0100

    feat: moved the docker home to /home/pi/ros_home. This way it's easier to access using SSH

--- OpenMowerOS/src/modules/openmoweros/filesystem/home/root/start_open_mower.sh
+++ OpenMowerOS/src/modules/openmoweros/filesystem/home/root/start_open_mower.sh
@@ -6,7 +6,7 @@ docker pull ghcr.io/clemenselflein/open_mower_ros:releases-${OM_VERSION}
 
 docker run \
 	-v /boot/mower_config.sh:/config/mower_config.sh \
-	-v /root/ros_home:/root/.ros\
+	-v /home/pi/ros_home:/root\
 	-v /root/rosconsole.config:/config/rosconsole.config\
 	-e ROSCONSOLE_CONFIG_FILE=/config/rosconsole.config\
 	-e ROSOUT_DISABLE_FILE_LOGGING=True\
