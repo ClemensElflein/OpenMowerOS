1f9717f18116284521067a60d87df6bd6e4b0de7
Clemens Elflein <clemens1@familie-elflein.de>
2022-11-07 23:12:47 +0100

    fix: we need to map /dev explicitly for gamepad hotplug to work

--- OpenMowerOS/src/modules/openmoweros/filesystem/home/root/start_open_mower.sh
+++ OpenMowerOS/src/modules/openmoweros/filesystem/home/root/start_open_mower.sh
@@ -8,6 +8,7 @@ docker run \
 	-v /boot/mower_config.sh:/config/mower_config.sh \
 	-v /home/pi/ros_home:/root\
 	-v /root/rosconsole.config:/config/rosconsole.config\
+	-v /dev:/dev\
 	-e ROSCONSOLE_CONFIG_FILE=/config/rosconsole.config\
 	-e ROSOUT_DISABLE_FILE_LOGGING=True\
 	--network="host"\
