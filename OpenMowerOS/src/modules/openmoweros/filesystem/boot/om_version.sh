3c4855219b3e888391155731aee47b074d5e5a22
Clemens Elflein <clemens1@familie-elflein.de>
2022-11-20 12:14:55 +0100

    moved src to correct location

--- OpenMowerOS/src/modules/openmoweros/filesystem/boot/om_version.sh
+++
@@ -1,6 +0,0 @@
-# select which version of the OpenMower software to run.
-# testing: This one changes very often. Only use it if you want to take part in the development or are asked to use it by a developer.
-# alpha: For people who like to test stuff. This one will be updated fairly often as well and without notice. Only use it, if you want to be among the first people to get new features, but prepare to have issues.
-# beta: This one is the _most stable_ one of the three. I try to keep this as stable as possible.
-
-export OM_VERSION="beta"
--- src/modules/openmower/filesystem/root/home/pi/start_ros_bash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Starts a bash in the docker container.
-# Use this for ROS specific commands (e.g. rostopic echo)
-
-docker exec -it open-mower /openmower_entrypoint.sh /bin/bash
