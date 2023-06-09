c52dbb1ffd582b05876de7a9cc91a940f7b386ad
Pavels Veretennikovs <pavels.veretennikovs@ness.com>
2023-06-09 22:37:08 +0300

    feat: allow invoking ros_bash with command
    ./start_ros_bash.sh rostopic echo /mower/status

--- OpenMowerOS/src/modules/openmower/filesystem/root/home/pi/start_ros_bash.sh
+++ OpenMowerOS/src/modules/openmower/filesystem/root/home/pi/start_ros_bash.sh
@@ -2,4 +2,4 @@
 # Starts a bash in the docker container.
 # Use this for ROS specific commands (e.g. rostopic echo)
 
-sudo podman exec -it openmower /openmower_entrypoint.sh /bin/bash
+sudo podman exec -it openmower /openmower_entrypoint.sh ${@:-/bin/bash}
