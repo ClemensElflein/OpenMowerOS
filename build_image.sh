3b2d22211d59f6f820090587b635ad78227acc2e
Pavel Veretennikov <vermut@pm.me>
2024-06-01 20:22:13 +0300

    feat: introduce semver and get rid of alpha/beta

--- build_image.sh
+++ build_image.sh
@@ -4,7 +4,7 @@
 if compgen -G "./OpenMowerOS/src/image/*" > /dev/null; then
     echo "Image exists, skipping download."
 else
-    echo "No image found, downloading latest Raspbiann image."
+    echo "No image found, downloading latest Raspbian image."
     bash -c "cd OpenMowerOS/src && mkdir -p image && cd image && wget -c --trust-server-names 'https://downloads.raspberrypi.org/raspios_lite_arm64_latest'"
 fi
 
