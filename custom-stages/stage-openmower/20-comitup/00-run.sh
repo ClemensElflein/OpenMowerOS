bb1f4d67069558640cbd9b8e7e00f3faa7653b7e
Jörg Ebeling <joerg@ebeling.ws>
2025-08-28 23:19:02 +0200

    Apply custom settings

--- custom-stages/stage-openmower/20-comitup/00-run.sh
+++ custom-stages/stage-openmower/20-comitup/00-run.sh
@@ -7,3 +7,4 @@ STAGE_DIR="$(dirname "$0")"
 # Install files
 install -m 0644 -D "$STAGE_DIR/files/etc/NetworkManager/conf.d/10-comitup.conf" "$ROOTFS_DIR/etc/NetworkManager/conf.d/10-comitup.conf"
 install -m 0644 -D "$STAGE_DIR/files/etc/systemd/system/comitup-nm-wifi-ensure.service" "$ROOTFS_DIR/etc/systemd/system/comitup-nm-wifi-ensure.service"
+install -m 0755 -D "$STAGE_DIR/files/usr/lib/raspberrypi-sys-mods/imager_custom" "$ROOTFS_DIR/usr/lib/raspberrypi-sys-mods/imager_custom"
