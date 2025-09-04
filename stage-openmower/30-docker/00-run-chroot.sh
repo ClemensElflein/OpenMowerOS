87129350c8855747c3c8461b2b942794517c0b9c
Jörg Ebeling <joerg@ebeling.ws>
2025-09-05 01:22:31 +0200

    Switch do official docker version instead of old Debian packaged

--- stage-openmower/30-docker/00-run-chroot.sh
+++ stage-openmower/30-docker/00-run-chroot.sh
@@ -1,10 +1,21 @@
 #!/bin/bash -e
 
 export DEBIAN_FRONTEND=noninteractive
+
 apt-get update
 
-# Install Docker engine from Debian
-apt-get install -y --no-install-recommends docker.io docker-cli
+# Set up Docker's official APT repository
+apt-get install -y --no-install-recommends ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+CODENAME="$(. /etc/os-release && echo "$VERSION_CODENAME")"
+echo "deb [arch=arm64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian ${CODENAME} stable" > /etc/apt/sources.list.d/docker.list
+
+apt-get update
+apt-get install -y --no-install-recommends \
+docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Enable the Docker daemon
 systemctl enable docker.service || true
