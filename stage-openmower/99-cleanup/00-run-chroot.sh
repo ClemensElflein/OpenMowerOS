7b96bbf4aa47ad7285f1001aeea3ac8fc86e252c
Jörg Ebeling <joerg@ebeling.ws>
2025-09-05 22:02:03 +0200

    Drop some unnecessary package

--- stage-openmower/99-cleanup/00-run-chroot.sh
+++ stage-openmower/99-cleanup/00-run-chroot.sh
@@ -3,5 +3,6 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # Final tidy-up to keep the image lean
+apt-get -y purge apt-listchanges || true
 apt-get -y autoremove --purge || true
 apt-get -y clean || true
