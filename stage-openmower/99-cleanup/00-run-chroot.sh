07a227f1fbc2463df8be7c30250147e5c5122c3a
Jörg Ebeling <joerg@ebeling.ws>
2025-09-06 00:12:16 +0200

    Cleanup of useless packages

--- stage-openmower/99-cleanup/00-run-chroot.sh
+++ stage-openmower/99-cleanup/00-run-chroot.sh
@@ -3,6 +3,6 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # Final tidy-up to keep the image lean
-apt-get -y purge apt-listchanges || true
+apt-get -y purge polkitd modemmanager ppp mkvtoolnix ntfs-3g exfatprogs || true
 apt-get -y autoremove --purge || true
 apt-get -y clean || true
