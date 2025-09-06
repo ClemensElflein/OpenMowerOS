aa5aabfbccc08f58ec6399fc42109ca5d00cf42a
Jörg Ebeling <joerg@ebeling.ws>
2025-09-06 15:11:27 +0200

    Improve file permissions

--- stage-openmower/40-openmower/00-run-chroot.sh
+++ stage-openmower/40-openmower/00-run-chroot.sh
@@ -3,11 +3,8 @@
 # Create ROS home
 mkdir -p /home/openmower/ros_home
 
-# Create empty config file for comptibility reason
-#touch /home/openmower/mower_config.sh
-
-chown -R openmower:openmower /home/openmower/
-chmod -R u=rwX,g=rwX,o=rX /home/openmower/
+chown -R openmower:openmower /home/openmower/ /opt/stacks/openmower/
+chmod -R u=rwX,g=rwX,o=rX /home/openmower/ /opt/stacks/openmower/
 
 # Make sure user is in docker group for user-run service
 usermod -aG docker openmower
