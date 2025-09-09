17c2c776d87469e26c600b92ba75f4b0691a60c6
Jörg Ebeling <joerg@ebeling.ws>
2025-09-09 23:02:38 +0200

    Remove obsolete files

--- OpenMowerOS/src/modules/openmower/filesystem/root/home/pi/start_ros_bash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Starts a bash in the docker container.
-# Use this for ROS specific commands (e.g. rostopic echo)
-
-sudo podman exec -it openmower /openmower_entrypoint.sh ${@:-/bin/bash}
