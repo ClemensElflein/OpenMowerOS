b70b55ba70fb40a0f680cb6181f6ac0823725a54
Jörg Ebeling <joerg@ebeling.ws>
2025-09-14 20:39:08 +0200

    Fix static openmower username

--- stage-openmower/15-pi-config/00-run.sh
+++ stage-openmower/15-pi-config/00-run.sh
@@ -6,4 +6,5 @@ STAGE_DIR="$(dirname "$0")"
 
 # Install files
 install -m 0755 -D "$STAGE_DIR/files/usr/lib/raspberrypi-sys-mods/imager_custom" "$ROOTFS_DIR/usr/lib/raspberrypi-sys-mods/imager_custom"
+install -m 0755 -D "$STAGE_DIR/files/usr/lib/userconf-pi/userconf" "$ROOTFS_DIR/usr/lib/userconf-pi/userconf"
 install -m 0755 -D "$STAGE_DIR/files/boot/firmware/config.addendum" "$ROOTFS_DIR/boot/firmware/config.addendum"
