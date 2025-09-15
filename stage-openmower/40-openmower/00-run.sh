1424d0f8e933b0e616418cd44de9be587d1f146a
Clemens Elflein <clemens1@familie-elflein.de>
2025-09-16 01:24:48 +0200

    migrate back to a ros_home folder

--- stage-openmower/40-openmower/00-run.sh
+++ stage-openmower/40-openmower/00-run.sh
@@ -5,7 +5,7 @@
 STAGE_DIR="$(dirname "$0")"
 
 # Install files
-install -m 0664 -D "$STAGE_DIR/files/home/openmower/mower_params.yaml" "$ROOTFS_DIR/home/openmower/mower_params.yaml"
+install -m 0664 -D "$STAGE_DIR/files/home/openmower/mower_params.yaml" "$ROOTFS_DIR/home/openmower/ros_home/mower_params.yaml"
 install -m 0755 -d "$ROOTFS_DIR/opt/stacks/openmower"
 install -m 0664 -D "$STAGE_DIR/files/opt/stacks/openmower/compose.yaml" "$ROOTFS_DIR/opt/stacks/openmower/compose.yaml"
 install -m 0644 -D "$STAGE_DIR/files/opt/stacks/openmower/.env" "$ROOTFS_DIR/opt/stacks/openmower/.env"
